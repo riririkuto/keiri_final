b93cb76c30e8d480933bce3c7f1b9b895f88d6b2
rikutoNext <127699589+rikutoNext@users.noreply.github.com>
2023-05-20 18:26:42 +0900

    a

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,16 +1,13 @@
-// #import "AppDelegate.h"
-// #import "GeneratedPluginRegistrant.h"
+import UIKit
+import Flutter
 
-@import Firebase;
-
-@implementation AppDelegate
-
-- (BOOL)application:(UIApplication *)application
-    didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-  [GeneratedPluginRegistrant registerWithRegistry:self];
-  [FIRApp configure];
-  // Override point for customization after application launch.
-  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+@UIApplicationMain
+@objc class AppDelegate: FlutterAppDelegate {
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    GeneratedPluginRegistrant.register(with: self)
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
 }
-
-@end
